aedc8d419dd3772931caa312a3c2cdf96df92a05
Dari Goldman <darigoldman@gmail.com>
2022-11-22 19:29:15 +0100

    fix: removed address auto-complete controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,9 +4,6 @@
 
 import { application } from "./application"
 
-import AddressAutocompleteController from "./address_autocomplete_controller"
-application.register("address-autocomplete", AddressAutocompleteController)
-
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
