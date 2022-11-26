2c4757971701e13c6843e7a4fe1746cfc8dcf8f4
Zsuzsanna Orehoczki <106694908+zynolia@users.noreply.github.com>
2022-11-26 00:05:45 +0000

    added new alert form

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -40,7 +40,9 @@ class AlertsController < ApplicationController
 
   def create
     @alert = Alert.create(alert_params)
-    @alert.user = current_user
+    @alert.upvotes = 0
+    @alert.status = 0
+    @alert.creator_id = current_user.id
     if @alert.save
       redirect_to alert_path(@alert), notice: "Thank you for sending your alert."
     else
