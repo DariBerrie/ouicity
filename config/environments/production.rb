051cab06389a5ee927e94190dbff5e8dbab3794d
Dari Goldman <darigoldman@gmail.com>
2022-11-30 13:28:06 +0100

    temporary: removed ssl forcing config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.force_ssl = true
-
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
