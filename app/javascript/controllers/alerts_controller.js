876dac422bdd6589aec8e8527b25a01361fb6634
Dari Goldman <darigoldman@gmail.com>
2022-12-02 12:04:05 +0100

    fix: fixed alert banner cards to match cards created from map js controller. added arrow icon back to alerts heading.

--- app/javascript/controllers/alerts_controller.js
+++ app/javascript/controllers/alerts_controller.js
@@ -3,10 +3,11 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["list"];
   connect() {
+
     this.showAlertList();
   }
 
-  showAlertList() {
+  showAlertList(event) {
     this.listTarget.classList.toggle("hidden");
   }
 }
