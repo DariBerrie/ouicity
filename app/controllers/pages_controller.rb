33df5bc64c5da4b03e9139257495611845dbdf5e
Dari Goldman <darigoldman@gmail.com>
2022-12-09 20:05:55 +0100

    fix: redirect worker to alerts#index upon login instead of homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    redirect_to alerts_path if current_user.role == "worker"
     @contact = Contact.new
     @alerts = Alert.all
     @markers = @alerts.geocoded.map do |alert|
