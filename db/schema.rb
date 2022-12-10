6ef904e987efd1895eebd506998a5a4b865a551f
Zsuzsanna Orehoczki <106694908+zynolia@users.noreply.github.com>
2022-12-10 15:48:31 +0000

    status change looks prettier

--- db/schema.rb
+++ db/schema.rb
@@ -99,15 +99,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_10_133950) do
     t.index ["worker_id"], name: "index_messages_on_worker_id"
   end
 
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
-  end
-
   create_table "subscribers", force: :cascade do |t|
     t.bigint "subscriber_id", null: false
     t.datetime "created_at", null: false
