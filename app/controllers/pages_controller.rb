15b52a227beb94b171a81b682593f27b2a24c83f
Dari Goldman <darigoldman@gmail.com>
2022-12-10 10:57:25 +0100

    fix: removed redirect from pages controller for now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    redirect_to alerts_path if current_user.role == "worker"
     @contact = Contact.new
     @alerts = Alert.all
     @markers = @alerts.geocoded.map do |alert|
