d355bca6dab15f3aec38f0d4f4fddbf17c595eb4
Dari Goldman <darigoldman@gmail.com>
2022-12-10 17:14:17 +0100

    fix: chat fixes

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -6,7 +6,9 @@ export default class extends Controller {
   static values = { chatroomId: Number };
   static targets = ["messages"];
   connect() {
-    this.channel = createConsumer().subscriptions.create({ channel: "ChatroomChannel", id: this.chatroomIdValue }, { received: (data) => this.#insertMessageAndScrollDown(data) });
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: (data) => this.#insertMessageAndScrollDown(data) });
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`);
   }
 
