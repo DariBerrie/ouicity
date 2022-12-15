e7f4c73cd699c3f204409888889fd93606d1667f
Dari Goldman <darigoldman@gmail.com>
2022-12-15 11:44:22 +0100

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_10_133950) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_13_085154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -97,6 +97,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_10_133950) do
     t.datetime "updated_at", null: false
     t.index ["assignment_id"], name: "index_messages_on_assignment_id"
     t.index ["worker_id"], name: "index_messages_on_worker_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "recipient_type", null: false
+    t.bigint "recipient_id", null: false
+    t.string "type", null: false
+    t.jsonb "params"
+    t.datetime "read_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["read_at"], name: "index_notifications_on_read_at"
+    t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
