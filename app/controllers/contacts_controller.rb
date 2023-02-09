dd66eaa00f7b13424d41593410121142e66dff17
Dari Goldman <darigoldman@gmail.com>
2023-02-09 13:58:30 -0500

    fix: added contact params

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,20 @@
 ## This is for the contact form on the homepage
 class ContactsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:create]
+  skip_before_action :authenticate_user!, only: :create
 
   def create
-    @contact = Contact.new(params[:contact])
+    @contact = Contact.new(contact_params)
     @contact.request = request
     if @contact.deliver
       redirect_to root_path, notice: "Message sent!"
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message, :nickname)
   end
 end
