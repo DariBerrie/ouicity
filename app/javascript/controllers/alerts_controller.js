7570bb380d28f7f2f762f726ba4af3799cfc3b87
Dari Goldman <darigoldman@gmail.com>
2024-01-28 12:13:00 +0100

    revert importmaps clean up maps controller, add listing html template

--- app/javascript/controllers/alerts_controller.js
+++ app/javascript/controllers/alerts_controller.js
@@ -1,15 +1,15 @@
-// import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus";
 
-// export default class extends Controller {
-//   static targets = ["list"];
-//   connect() {
-//     this.showAlertList();
-//   }
+export default class extends Controller {
+  static targets = ["list"];
+  connect() {
+    this.showAlertList();
+  }
 
-//   showAlertList(event) {
-//     // Window.scrollTo({
-//     //   top: this.listTarget.scrollTop,
-//     //   behavior: 'smooth'
-//     // })
-//   }
-// }
+  showAlertList(event) {
+    Window.scrollTo({
+      top: this.listTarget.scrollTop,
+      behavior: 'smooth'
+    })
+  }
+}
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -11,6 +11,7 @@ export default class extends Controller {
   static targets = ["map"]
 
   connect() {
+    console.log("map controller connected")
     mapboxgl.accessToken = this.apiKeyValue
 
     this.map = new mapboxgl.Map({
@@ -19,56 +20,12 @@ export default class extends Controller {
     })
 
     this.#addMarkersToMap()
+    console.log("end of add markers")
     this.#fitMapToMarkers()
+    console.log("end of fit map")
+    this.#geocoderSearch()
+    console.log("end of geocoder")
 
-    const geocoder = new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-                                          countries: 'fr',
-                                          mapboxgl: mapboxgl })
-
-    geocoder.options.placeholder = "Enter your address..."
-
-    if (document.getElementsByClassName('mapboxgl-ctrl-geocoder').length === 0) {
-      document.getElementById('geocoder').appendChild(geocoder.onAdd(this.map))
-    }
-
-    geocoder.on('result', (event) => {
-      sessionStorage.setItem("addressEvent", event.result.place_name)
-      const searchResult = event.result.geometry
-      const options = { units: 'kilometers' }
-      const alerts = this.alertsValue
-      alerts.forEach((alert) => {
-        const alertPoint = new mapboxgl.Point(alert.longitude, alert.latitude)
-        alert.distance = turf.distance(
-          searchResult,
-          turf.point([alert.longitude, alert.latitude]),
-          options
-        )
-      })
-
-      alerts.sort((a, b) => {
-        if (a.distance > b.distance) {
-          return 1;
-        }
-        if (a.distance < b.distance) {
-          return -1;
-        }
-        return 0; // a must be equal to b
-      })
-
-      const nearbyAlerts = []
-      alerts.forEach((alert) => {
-        if ((Math.round(alert.distance * 100) / 100) <= 2){
-          nearbyAlerts.push(alert)
-        }
-      })
-
-      const listings = document.getElementById('listings');
-      while (listings.firstChild) {
-        listings.removeChild(listings.firstChild);
-      }
-      this.#buildAlertList(nearbyAlerts)
-      this.#fitMapToAlerts(nearbyAlerts, searchResult)
-    })
   }
 
   #addMarkersToMap(){
@@ -96,36 +53,87 @@ export default class extends Controller {
   }
 
   #buildAlertList(alerts) {
+    const recentAlerts = document.querySelector('.recent-alerts')
+    const alertsHeading = document.getElementById('alerts-heading')
+    this.buildAlertsHeading()
+  
     alerts.forEach((alert) => {
-      const listings = document.getElementById('listings')
-      const listing = listings.appendChild(document.createElement('div'))
-      listing.id = `listing-${alert.id}`
-      listing.className = "item";
-      const roundedDistance = Math.round(alert.distance * 100) / 100;
-      const alertsHeading = document.getElementById('alerts-heading')
-      this.buildAlertsHeading()
       const alertUrl = document.getElementById('alert-path')
-      listing.innerHTML = `
-        <div class="listing card shadow-sm d-flex flex-row">
-          <img class="me-3"
-               src="${alert.photos[0].url}"
-               style="width:80px; height:80px; border-radius:50%; object-fit:fill;">
-          <div class="listing-details">
-            <a href="${alertUrl.value}/${alert.id}">${alert.address}</a><br>
-            <strong>${alert.title}</strong>
-            <p class="fs-6">${alert.description}<br>
-            <strong>${roundedDistance} kms away</strong></p>
-          </div>
-        </div>`
+      const roundedDistance = Math.round(alert.distance * 100) / 100;
+
+      const listingTemplate = document.getElementById("listingTemplate")
+      console.log("listing temp", listingTemplate)
+      const clone = listingTemplate.content.cloneNode(true)
+      clone.id = `listing-${alert.id}`
+      clone.querySelector("img").src = alert.photos[0].url
+      clone.querySelector("a").href = alertUrl.value + "/" + alert.id
+      clone.querySelector("a").textContent = alert.address
+      console.log("title", clone.querySelector("span.title").textContent)
+      clone.querySelector("span.title").textContent = alert.title
+      clone.querySelector("p.description").textContent = alert.description
+      clone.querySelector("span.distance").textContent = roundedDistance + " kms away"
+
+      recentAlerts.appendChild(clone)
     })
   }
+
   buildAlertsHeading(){
-    const alertsHeading = document.getElementById('alerts-heading')
-    alertsHeading.innerHTML= `
-      <h5 id="alerts-heading">Alerts in this area
-      <a data-action="click->alerts#showAlertList"
-      class="fs-3 toggle">
-      <i class="fa-solid fa-circle-chevron-down"></i></a></h5>`
+    document.getElementById("alerts-heading").innerText = "Alerts in this area"
+  }
+
+  #geocoderSearch() {
+    const geocoder = new MapboxGeocoder({ 
+      accessToken: mapboxgl.accessToken,
+      countries: 'fr',
+      mapboxgl: mapboxgl 
+    })
+    
+    const geocoderObj = document.getElementById('geocoder')
+    geocoder.options.placeholder = "Enter your address"
+
+    if (document.getElementsByClassName('mapboxgl-ctrl-geocoder').length === 0) {
+      geocoderObj.appendChild(geocoder.onAdd(this.map))
+    }
+
+    geocoder.on('result', (event) => {
+      sessionStorage.setItem("addressEvent", event.result.place_name)
+
+      const searchResult = event.result.geometry
+      const options = { units: 'kilometers' }
+      const alerts = this.alertsValue
+
+      alerts.forEach((alert) => {
+        const alertPoint = new mapboxgl.Point(alert.longitude, alert.latitude)
+        alert.distance = turf.distance(
+          searchResult,
+          turf.point([alert.longitude, alert.latitude]),
+          options
+        )
+      })
+
+      alerts.sort((a, b) => {
+        if (a.distance > b.distance) { return 1 }
+        else if (a.distance < b.distance) { return -1 }
+        else { return 0 } // a must be equal to b
+      })
+
+      const alertsInProximity = []
+
+      alerts.forEach((alert) => {
+        const nearbyDistance = 2
+        if ((Math.round(alert.distance * 100) / 100) <= nearbyDistance) {
+          alertsInProximity.push(alert)
+        }
+      })
+
+    const recentAlertsContainer = document.querySelector('.recent-alerts');
+    
+    while (recentAlertsContainer.firstChild) {
+      recentAlertsContainer.removeChild(recentAlertsContainer.firstChild);
+    }
+    this.#buildAlertList(alertsInProximity)
+    this.#fitMapToAlerts(alertsInProximity, searchResult)
+    })
   }
 
 }
